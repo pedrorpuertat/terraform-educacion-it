6743e256daed4ead3a7191b7f8105135afc8162e
Pedro Puerta <ppuerta1986@gmail.com>
2025-03-25 18:20:41 -0300

    ajustes de vpc

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 resource "aws_instance" "example" {
   ami           = "ami-084568db4383264d4"
   instance_type = "t2.micro"
+    subnet_id              = "subnet-xxxxxxxx" 
+    vpc_security_group_ids = ["subnet-057ae556cebf1fcf3"] 
+    associate_public_ip_address = true
+  tags = {
+    Name = "ec2-terraform"
+    environment = "educacionit"
+  }
 }

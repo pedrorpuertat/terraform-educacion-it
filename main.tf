11bc40410a9ffcc09b3bf3c99510a63d9fee90d8
Pedro Puerta <ppuerta1986@gmail.com>
2025-03-25 18:24:51 -0300

    cambio id vpc

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "example" {
   ami           = "ami-084568db4383264d4"
   instance_type = "t2.micro"
-    subnet_id              = "subnet-xxxxxxxx" 
-    vpc_security_group_ids = ["subnet-057ae556cebf1fcf3"] 
+    subnet_id              = "subnet-057ae556cebf1fcf3" 
+    vpc_security_group_ids = ["sg-030815cd88790dad7"] 
     associate_public_ip_address = true
   tags = {
     Name = "ec2-terraform"

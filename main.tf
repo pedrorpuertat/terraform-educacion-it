92cac922550863f779517a82a3d3da5bf90eab8e
Pedro Puerta <ppuerta1986@gmail.com>
2025-03-25 20:25:35 -0300

    Ajustes personales

--- main.tf
+++ main.tf
@@ -1,54 +1,12 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-  required_version = ">= 1.1.0"
-
-  cloud {
-    organization = "EducacionIT-Bootcamp"
-
-    workspaces {
-      name = "terraform-educacion-it"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-west-1"
-}
-
-resource "random_pet" "sg" {}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  tags = {
+    Name = "ec2-terraform-web"
+    environment = "educacionit"
+  }
 
   user_data = <<-EOF
               #!/bin/bash
@@ -61,7 +19,7 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
+  name = "${random_pet.sg.id}-sg-educacion-it"
   ingress {
     from_port   = 8080
     to_port     = 8080

8560103de89db7db71af7a6919b5f5bd0c1fc100
Pedro Puerta <ppuerta1986@gmail.com>
2025-03-25 20:45:12 -0300

    cambio

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_vpc" "vpc-sg" {
 resource "aws_subnet" "subnet-sg" {
   vpc_id                  = aws_vpc.vpc-sg.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2b"
+  availability_zone       = "us-west-1b"
   map_public_ip_on_launch = true
 }
 

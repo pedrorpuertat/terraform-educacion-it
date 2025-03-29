cd69a1d8ac3a3c5756c60b0dd7338d378dfc5128
Pedro Puerta <ppuerta1986@gmail.com>
2025-03-29 12:48:21 -0300

    Cambios

--- main.tf
+++ main.tf
@@ -16,56 +16,30 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_vpc" "vpc-sg" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-}
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-resource "aws_subnet" "subnet-sg" {
-  vpc_id                  = aws_vpc.vpc-sg.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.vpc-sg.id
-}
-
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.vpc-sg.id
-}
-
-resource "aws_route" "public_rt_route" {
-  route_table_id         = aws_route_table.public_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.gw.id
-}
-
-resource "aws_route_table_association" "public_rt_assoc" {
-  subnet_id      = aws_subnet.subnet-sg.id
-  route_table_id = aws_route_table.public_rt.id
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
 }
 
 resource "aws_security_group" "web-sg" {
-  vpc_id = aws_vpc.vpc-sg.id
-  name   = "${random_pet.sg.id}-sg-educacion-it"
-  
+  name = "${random_pet.sg.id}-sg"
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
   egress {
     from_port   = 0
     to_port     = 0
@@ -74,25 +48,6 @@ resource "aws_security_group" "web-sg" {
   }
 }
 
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-  subnet_id              = aws_subnet.subnet-sg.id
-
-  tags = {
-    Name        = "ec2-terraform-web"
-    environment = "educacionit"
-  }
-
-user_data = <<EOF
-              #!/bin/bash
-              sudo apt-get update
-              sudo apt-get install -y apache2
-              sudo systemctl restart apache2
-              EOF
-}
-
 output "web-address" {
-  value = "${aws_instance.web.public_dns}:80"
+  value = "${aws_instance.web.public_dns}:8080"
 }
--- versions.tf
+++ versions.tf
@@ -1,5 +1,16 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+
   cloud {
     organization = "EducacionIT-Bootcamp"
     workspaces {
@@ -9,5 +20,5 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }

95129cc321b0666bd18758cec0d0dc166c895bd6
pedrorpuertat <pedrorpuertat@gmail.com>
2025-03-29 13:21:36 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,33 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "EducacionIT-Bootcamp"
+
+    workspaces {
+      name = "terraform-educacion-it"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+
 resource "random_pet" "sg" {}
 
 data "aws_ami" "ubuntu" {
